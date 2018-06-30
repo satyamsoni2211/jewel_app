7da5ea76eee879d394fb44847e8bb49b0cf56a91
Satyam Soni <satyam.soni@livehealth.in>
2018-06-30 11:31:57 +0530

    changes in webpack config file

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -3,49 +3,46 @@ const webpack = require('webpack');
 
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const BitBarWebpackProgressPlugin = require("bitbar-webpack-progress-plugin");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const BundleTracker = require('webpack-bundle-tracker');
 
-const debug = false;
+const debug = true;
 
 
 module.exports = {
-    context: __dirname,
-    entry: {
-        centersDashboard: './components/support_dashboard/main.jsx',
-        planManager: './components/plan_management/planManager.jsx',
-        todo: './components/todo/index.jsx',
-        centersDashboardV2:'./components/accounts/accounts.jsx',
-        payment: './components/Payment/index.jsx',
-    },
-    devtool: debug ? 'inline-eval-cheap-source-map' : 'source-map',
-    output: {
-        //where you want your compiled bundle to be stored
-        path: path.resolve('./assets/'),
-        //naming convention webpack should use for your files
-        filename: '[name].react-compiled.js'
+	context: __dirname,
+	entry: {
+		App: './src/index.js'
+	},
+	// devtool: debug ? 'inline-eval-cheap-source-map' : 'source-map',
+	output: {
+		//where you want your compiled bundle to be stored
+		path: path.resolve('../static/assets/'),
+		//naming convention webpack should use for your files
+		filename: '[name].react-compiled[hash].js'
 
-    },
-    resolve: {
+	},
+	resolve: {
 
-        modules: [
-            path.join(__dirname, "src"),
-            "node_modules"
-        ]
-    },
-    module: {
-        loaders: [
-            {
-                test: /(\.js|\.jsx)$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                options: {
-                    babelrc: false,
-                    presets: [
-                        ['es2015', {modules: false}],
-                        'react',
-                    ],
-                }
-            }, {
+		modules: [
+			"node_modules"
+		]
+	},
+	module: {
+		rules: [
+			{
+				test: /(\.js|\.jsx)$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				options: {
+					babelrc: false,
+					presets: [
+						'env',
+						'react',
+						'stage-0',
+					],
+				}
+			}, {
 				test: /\.css$/,
 				use: [
 					require.resolve('style-loader'),
@@ -75,26 +72,49 @@ module.exports = {
 					},
 				],
 			},
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack?{progressive:true, optimizationLevel: 3, interlaced: false, pngquant:{quality: "30-40", speed: 1}}'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin({filename: './[name].react.css', disable: false, allChunks: true}),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': debug ? JSON.stringify('development') : JSON.stringify('production')
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        }),
-    ]
+			// {
+			// 	test: /\.(jpe?g|png|gif|svg)$/i,
+			// 	loaders: [
+			// 		'file?hash=sha512&digest=hex&name=[hash].[ext]',
+			// 		'image-webpack?{progressive:true, optimizationLevel: 3, interlaced: false, pngquant:{quality: "30-40", speed: 1}}'
+			// 	]
+			// }{
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							bypassOnDebug: true,
+							publicPath: '../static/assets/'
+						}
+					}
+				],
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin({ filename: './[name].react.css', disable: false, allChunks: true }),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': debug ? JSON.stringify('development') : JSON.stringify('production')
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compressor: {
+				warnings: false
+			},
+			output: {
+				comments: false
+			},
+			minimize: true
+
+		}),
+		new BundleTracker({filename: './webpack-stats.json'})
+		// new MinifyPlugin()
+		// new webpack.DefinePlugin({
+		// 	'process.env.NODE_ENV': JSON.stringify('production')
+		//   }),
+		// new webpack.optimize.UglifyJsPlugin()
+	]
 };
