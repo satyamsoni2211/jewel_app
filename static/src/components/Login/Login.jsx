fac51b973c107e1757d4226eaaba776bcf04194d
Satyam Soni <satyam.soni@livehealth.in>
2018-07-01 12:03:56 +0530

    login form

--- static/src/components/Login/Login.jsx
+++ static/src/components/Login/Login.jsx
@@ -9,7 +9,7 @@ import {
     Modal,
     ModalBody,
     ModalFooter,
-    ModalHeader
+    FormText
 } from 'reactstrap';
 import { Aux } from '../utilities/utilities.jsx';
 
@@ -39,7 +39,7 @@ class Login extends Component {
                                 <div className="col-md-6">
                                     <small className='text-info pull-right'
                                         style={{ cursor: 'pointer' }}
-                                        onClick={()=> {this.setState({modal: !this.state.modal})}}>Dont have account ? Signup</small>
+                                        onClick={() => { this.setState({ modal: !this.state.modal }) }}>Dont have account ? Signup</small>
                                 </div>
                             </div>
                             <Button className='btn btn-outline-dark mt-3'>Submit</Button>
@@ -47,14 +47,97 @@ class Login extends Component {
                     </div>
                 </div>
                 <Modal isOpen={this.state.modal}>
-                    <ModalHeader>Modal title</ModalHeader>
                     <ModalBody className='bg-dark text-light'>
-                        <b>Look at the top right of the page/viewport!</b><br />
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                     </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                        <Button color="secondary" onClick={() => this.setState({modal: !this.state.modal})}>Cancel</Button>
+                        <div className="col-md-12 p-4">
+                            <h3 className='text-uppercase text-center'>Signup for Jewel app</h3>
+                            <hr className='clear-both' />
+                            <Form>
+                                <FormGroup>
+                                    <Label for="exampleEmail">Email</Label>
+                                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="examplePassword">Password</Label>
+                                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleUrl">Url</Label>
+                                    <Input type="url" name="url" id="exampleUrl" placeholder="url placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleNumber">Number</Label>
+                                    <Input type="number" name="number" id="exampleNumber" placeholder="number placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleDatetime">Datetime</Label>
+                                    <Input type="datetime" name="datetime" id="exampleDatetime" placeholder="datetime placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleDate">Date</Label>
+                                    <Input type="date" name="date" id="exampleDate" placeholder="date placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleTime">Time</Label>
+                                    <Input type="time" name="time" id="exampleTime" placeholder="time placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleColor">Color</Label>
+                                    <Input type="color" name="color" id="exampleColor" placeholder="color placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleSearch">Search</Label>
+                                    <Input type="search" name="search" id="exampleSearch" placeholder="search placeholder" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleSelect">Select</Label>
+                                    <Input type="select" name="select" id="exampleSelect">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleSelectMulti">Select Multiple</Label>
+                                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleText">Text Area</Label>
+                                    <Input type="textarea" name="text" id="exampleText" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleFile">File</Label>
+                                    <Input type="file" name="file" id="exampleFile" />
+                                    <FormText color="muted">
+                                        This is some placeholder block-level help text for the above input.
+                                        It's a bit lighter and easily wraps to a new line.
+                            </FormText>
+                                </FormGroup>
+                                <FormGroup check>
+                                    <Label check>
+                                        <Input type="radio" />{' '}
+                                        Option one is this and that—be sure to include why it's great
+                                </Label>
+                                </FormGroup>
+                                <FormGroup check>
+                                    <Label check>
+                                        <Input type="checkbox" />{' '}
+                                        Check me out
+                                </Label>
+                                </FormGroup>
+                            </Form>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter className='bg-dark text-light'>
+                        <Button className='btn btn-outline-light' onClick={this.toggle}>Do Something</Button>{' '}
+                        <Button className='btn btn-outline-light' onClick={() => this.setState({ modal: !this.state.modal })}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
             </Aux>
